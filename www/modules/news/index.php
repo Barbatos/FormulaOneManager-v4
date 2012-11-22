37a6163f6d25c1494454c6e25978372b83d40398
Charles Duprey <barbatos@urbanterror.info>
2012-11-22 18:20:40 +0100

    Added the licence (GNU GPL v3)

--- www/modules/news/index.php
+++ www/modules/news/index.php
@@ -1,4 +1,24 @@
 <?php
+/*
+ *  This file is part of Formula One Manager.
+ *
+ *  Formula One Manager is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Formula One Manager is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Formula One Manager. If not, see http://www.gnu.org/licenses/
+ *
+ *  @copyright  (c) Formula One Manager 2007/2012
+ *  @author     Charles 'Barbatos' Duprey
+ *  @email      barbatos@f1m.fr
+ */
 
 $L_news = loadIniFile(SITE_ROOT.'files/lang/'.$Bld['lang'].'/news.ini');
 
